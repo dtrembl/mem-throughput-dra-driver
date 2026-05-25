27457bc4855b81831dcd47652caefe74321d5a21
Daniel Tremblay <daniel.tremblay@riedel.net>
2026-05-25 15:41:58 -0400

    Small cleanup

--- scan.sh
+++ scan.sh
@@ -1,4 +1,4 @@
-for ns in basic-resourceclaimtemplate; do \
+for ns in basic-resourceclaimtemplate media; do \
   echo "${ns}:"
   for pod in $(kubectl get pod -n ${ns} --output=jsonpath='{.items[*].metadata.name}'); do \
     for ctr in $(kubectl get pod -n ${ns} ${pod} -o jsonpath='{.spec.containers[*].name}'); do \
